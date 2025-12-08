9fa4d7d50d1f510d4d7ea7df0ec7d11d8080fe29
Destruction Studios <founderofbeans@gmail.com>
2025-12-07 20:28:00 -0500

    added blinking locators

--- scenes/game/map/map.gd
+++ scenes/game/map/map.gd
@@ -1,7 +1,18 @@
 extends Control
 class_name Map
 
+@onready var blink_timer: Timer = $BlinkTimer
+
 var pengu_ai: PenguAI
 
 func _ready():
 	pass
+
+
+func _on_blink_timer_timeout() -> void:
+	for item: TextureRect in get_tree().get_nodes_in_group("locator"):
+		if !item.visible:
+			continue
+		var a = 0
+		if item.modulate.a == 0: a = 1
+		item.modulate.a = a
