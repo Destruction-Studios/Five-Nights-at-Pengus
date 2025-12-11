b9f596b3f7735be93e7b55ae1eb6dd9f4d4abe68
Destruction Studios <founderofbeans@gmail.com>
2025-12-11 13:58:19 -0500

    Fixed Cookiemaker

--- scenes/game/cookie_maker/bar/bar.gd
+++ scenes/game/cookie_maker/bar/bar.gd
@@ -4,23 +4,33 @@ class_name CookieMakerBar
 signal start_fill
 signal end_fill
 
+const BAR_HEIGHT := 450.0
+
 @onready var progress_bar: ProgressBar = $VBoxContainer/ProgressBar
 @onready var range_label: ColorRect = $VBoxContainer/ProgressBar/Range
 
 var acceptable_range: Array[float]
+var height = BAR_HEIGHT
 
-# BAR SIZE IS CHANGED CHANGE THIS!!!!!
-static func scale_number(num: float) -> float:
-	return 450.0/(100.0/num)
+func scale_number(num: float) -> float:
+	return (num / 100.0) * height
 
 func _ready() -> void:
-	print("\t\tSetting range: ", acceptable_range)
-	range_label.size.y = GameSettings.FILL_RANGE
-	range_label.position.y = scale_number(acceptable_range[1])
+	var h = progress_bar.size.y
+
+	var min_px = (acceptable_range[0] / 100.0) * h
+	var max_px = (acceptable_range[1] / 100.0) * h
+
+	# invert because bar visual fills from bottom
+	var inv_min = h - max_px
+	var inv_max = h - min_px
+
+	range_label.position.y = inv_min
+	range_label.size.y = inv_max - inv_min
+
 
 func _on_button_button_down() -> void:
 	start_fill.emit()
-
 
 func _on_button_button_up() -> void:
 	end_fill.emit()
--- scenes/game/cookie_maker/cookie_maker.gd
+++ scenes/game/cookie_maker/cookie_maker.gd
@@ -1,6 +1,8 @@
 extends Control
 class_name CookieMaker
 
+signal completed
+
 const BAR = preload("uid://cfd2vushe61th")
 
 @export var cookie_controller: CookieController
@@ -13,59 +15,61 @@ var current_down = null
 var is_active = true
 
 func _ready() -> void:
-	print("Init CookieMaker")
+	%CookieLabel.text = "= " + str(GameSettings.COOKIE_REWARD)
+	
 	for i in GameSettings.TOTAL_BARS:
-		print("\tCreating Bar: ", i)
-		
 		var a_range = create_range()
-		
+
 		var inst: CookieMakerBar = BAR.instantiate()
-		
 		inst.acceptable_range = a_range
+
 		inst.start_fill.connect(start_fill.bind(i))
 		inst.end_fill.connect(end_fill.bind(i))
-		
+
 		bars.append(inst)
 		progresses.append(0.0)
 		acceptable_ranges.append(a_range)
-		
+
 		$ProgressBars.add_child(inst)
 
 func _process(delta: float) -> void:
-	var in_range = 0
+	$Fade.visible = !is_active
 	
+	var in_range = 0
+
 	if is_active:
 		for i in bars.size():
-			var current_progress = progresses.get(i)
-			var bar = bars.get(i)
-			var acceptable_range = acceptable_ranges.get(i)
-			
+			var current_progress = progresses[i]
+			var bar = bars[i]
+			var acceptable_range = acceptable_ranges[i]
+
 			if current_down == i:
 				current_progress += GameSettings.FILL_INCREASE_RATE * delta
-			else: 
+			else:
 				current_progress -= GameSettings.FILL_DECREASE_RATE * delta
-			
+
 			current_progress = clampf(current_progress, 0.0, 100.0)
 			
 			if current_progress > acceptable_range[0] and current_progress < acceptable_range[1]:
 				in_range += 1
-			
-			progresses.set(i, current_progress)
-			
+
+			progresses[i] = current_progress
 			bar.set_progress(current_progress)
-	#print(in_range)
+
 	if in_range >= bars.size():
-		print("ALL READY!!!")
+		completed.emit()
+		is_active = false
 
 func create_range() -> Array[float]:
-	var max_r = randf_range(GameSettings.FILL_RANGE + randf_range(5.0, 7.5), 100.0 - randf_range(12.5, 15.0))
+	var max_r = randf_range(GameSettings.FILL_RANGE + randf_range(5.0, 7.5),
+		100.0 - randf_range(12.5, 15.0))
 	var min_r = max_r - GameSettings.FILL_RANGE
-	
+
 	if min_r < 0.0:
 		var up = abs(min_r) + 5.0
 		min_r += up
 		max_r += up
-	
+
 	return [min_r, max_r]
 
 func start_fill(bar: int) -> void:
